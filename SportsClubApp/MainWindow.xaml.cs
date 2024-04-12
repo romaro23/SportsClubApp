8dbe0fe03b057faad7b253fb1b940dab4a4772ac
Nahorniy Roman <nahornyi.r22@fpm.dnu.edu.ua>
2024-04-13 02:02:59 +0300

    Add project files.

---
+++ SportsClubApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SportsClubApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EmailInput.GotFocus += EmailGotFocus;
+            EmailInput.LostFocus += EmailLostFocus;
+            PasswordInput.GotFocus += PasswordGotFocus;
+            PasswordInput.LostFocus += PasswordLostFocus;
+        }
+        private void OpenNewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            HomePage window = new HomePage();
+            window.Show();
+            this.Close();
+        }
+        private void EmailGotFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).Text = "";
+        }
+        private void EmailLostFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).Text = "Enter your email address";
+        }
+        private void PasswordGotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordLabel.Content = "";
+        }
+        private void PasswordLostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordLabel.Content = "Enter your password";
+        }
+    }
+}
